71b6855a988eb7f8f1edc979feaf2aa031edfbee
Xidaru <72696223+Xidaru@users.noreply.github.com>
2022-05-06 00:26:40 +0300

    It will work now, I think
    Bruh, I'm deoing it all at 12 o clock.

--- pythonlogin/main.py
+++ pythonlogin/main.py
@@ -1,3 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+
 def login():
     # Output message if something goes wrong...
     msg = ''
